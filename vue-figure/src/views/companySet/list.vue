b54325b25eb01f083963777a17ed5c4c7516407c
D18130495 <D18130495@mytudublin.ie>
2022-01-28 19:49:39 +0000

    Company pagination

--- vue-figure/src/views/companySet/list.vue
+++ vue-figure/src/views/companySet/list.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="app-container">
-    company set
+    <el-form :inline="true" class="demo-form-inline" style="text-align: right;">
+      <el-form-item>
+        <el-input v-model="searchObj.compName" placeholder="Company Name" />
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="searchObj.compCode" placeholder="Company Code" />
+      </el-form-item>
+      <el-button type="primary" @click="getList()">Search</el-button>
+    </el-form>
+    <!-- table -->
+    <el-table :data="list" style="width: 100%">
+      <el-table-column type="index" label="index" />
+      <el-table-column prop="compName" label="Company Name" />
+      <el-table-column prop="compCode" label="Company Code" />
+      <el-table-column prop="apiUrl" label="API Url" />
+      <el-table-column prop="contactsName" label="Contant Name" />
+      <el-table-column prop="contactsPhone" label="Contant Phone" />
+      <el-table-column label="status">
+        <template slot-scope="scope">
+          {{ scope.row.status === 1 ? 'able' : 'disable' }}
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- pagination -->
+    <el-pagination
+      :current-page="current"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    />
   </div>
 </template>
 
@@ -10,7 +41,7 @@ import companySet from '@/api/companySet'
 export default {
   data() {
     return {
-      current: 1,
+      current: 0,
       limit: 3,
       total: 0,
       searchObj: {},
@@ -21,7 +52,8 @@ export default {
     this.getList()
   },
   methods: {
-    getList() {
+    getList(page = 1) {
+      this.current = page
       companySet.getCompanySetList(this.current, this.limit, this.searchObj)
         .then(response => {
           this.list = response.data.records
